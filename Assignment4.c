188769bcff6ae38766af2e63d4f1ee287d716660
Prajusha-Rao <www.prajusharao8@gmail.com>
2025-01-18 15:25:47 +0530

    Add files via upload

---
+++ Assignment4.c
@@ -0,0 +1,21 @@
+#include <stdio.h> //to check if the character is digit,etc
+
+int main()
+{
+char ch;
+    printf("Enter any character ");
+scanf("%c",&ch);
+if((ch>='a' && ch<='z')||(ch>='A' && ch<='Z'))
+{
+    printf("The character '%c' is an alphabet",ch);
+}
+else if(ch>='0' && ch<='9')
+{
+     printf("The character '%c' is a number",ch);
+}
+else
+{
+   printf("The character '%c' is a special character");
+}
+    return 0;
+}
